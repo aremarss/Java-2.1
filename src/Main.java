442e3651a7bb967b0e317061e9b67de182b2861b
aremarss <aremars@mail.ru>
2021-10-06 19:34:39 +0300

    Initizalization

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 12000; // Подставить любую стоимость билета.
+        int bonusMiles = 20;
+        int totalMiles = ticketPrice / bonusMiles;
+
+        System.out.println("Бонус: " + totalMiles + " миль.");
+
+    }
+}
